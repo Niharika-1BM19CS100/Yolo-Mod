ba65d6fb774bf9ee9b2be930d11bf9c84ae309f7
Niharika-1BM19CS100 <71308403+Niharika-1BM19CS100@users.noreply.github.com>
2023-04-20 23:15:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,29 +1,30 @@
 import albumentations as A
 import cv2
 import torch
-import os
-from albumentations.pytorch import ToTensorV2
 
-path = os.getcwd()
+from albumentations.pytorch import ToTensorV2
+from utils import seed_everything
+
 DATASET = 'trashnet'
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+# seed_everything()  # If you want deterministic behavior
 NUM_WORKERS = 4
 BATCH_SIZE = 32
 IMAGE_SIZE = 416
 NUM_CLASSES = 6
 LEARNING_RATE = 1e-5
 WEIGHT_DECAY = 1e-4
-NUM_EPOCHS = 12
+NUM_EPOCHS = 100
 CONF_THRESHOLD = 0.05
 MAP_IOU_THRESH = 0.5
 NMS_IOU_THRESH = 0.45
 S = [IMAGE_SIZE // 32, IMAGE_SIZE // 16, IMAGE_SIZE // 8]
 PIN_MEMORY = True
-LOAD_MODEL = False
-SAVE_MODEL = False
+LOAD_MODEL = True
+SAVE_MODEL = True
 CHECKPOINT_FILE = "checkpoint.pth.tar"
-IMG_DIR = path + "//" + DATASET + "/images/"
-LABEL_DIR = path + "//" + DATASET + "/images/"
+IMG_DIR = DATASET + "/images/"
+LABEL_DIR = DATASET + "/images/"
 
 ANCHORS = [
     [(0.28, 0.22), (0.38, 0.48), (0.9, 0.78)],
