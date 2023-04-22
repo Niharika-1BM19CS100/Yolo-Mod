b86b2938b3178a28167aa06b2d6303404c2c78d2
Niharika-1BM19CS100 <71308403+Niharika-1BM19CS100@users.noreply.github.com>
2023-04-22 18:46:12 +0530

    Update train.py

--- train.py
+++ train.py
@@ -2,9 +2,28 @@
 Main file for training Yolo model on Pascal VOC and COCO dataset
 """
 
+
 import config
 import torch
 import torch.optim as optim
+
+from model import YOLOv3
+from tqdm import tqdm
+from utils import (
+    mean_average_precision,
+    cells_to_bboxes,
+    get_evaluation_bboxes,
+    save_checkpoint,
+    load_checkpoint,
+    check_class_accuracy,
+    get_loaders,
+    plot_couple_examples
+)
+from loss import YoloLoss
+import warnings
+warnings.filterwarnings("ignore")
+
+torch.backends.cudnn.benchmark = True
 
 from model import YOLOv3
 from tqdm import tqdm
